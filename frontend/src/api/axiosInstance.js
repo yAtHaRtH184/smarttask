2f9d1eddf356c7c9ec850c100a7981b1c5bf3d17
yAtHaRtH184 <rana.yatharth1804@gmail.com>
2026-07-11 10:34:16 +0530

    Use environment variable for API URL

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: { 'Content-Type': 'application/json' },
 });
 
